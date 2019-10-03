1eebf43c606b3ca551a0467d162b480708d5584b
David <darensr@gmail.com>
2019-10-03 22:17:50 +0200

    Add style and close menu to Link

--- packages/wc-sevilla-theme/src/Link.js
+++ packages/wc-sevilla-theme/src/Link.js
@@ -1,20 +1,25 @@
 import React from "react";
-import { connect } from "frontity";
+import { connect, styled } from "frontity";
 
 const Link = ({ href, actions, children }) => {
   return (
     <div>
-      <a
+      <Anchor
         href={href}
         onClick={event => {
           event.preventDefault();
           actions.router.set(href);
+          actions.theme.closeMenu();
         }}
       >
         {children}
-      </a>
+      </Anchor>
     </div>
   );
 };
 
 export default connect(Link);
+
+const Anchor = styled.a`
+  color: steelblue;
+`;
